f68f219753d2ed7b901acd1a9534aed13bc745a5
giotroni <giorgio@troni.it>
2015-05-29 17:15:01 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -11,21 +11,23 @@ var app = {
   },
   onDeviceReady: function(){
     alert("carico la mappa");
-    $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBH7uaEdJNrfDU4RHjgtPg971Fm8pHzZ3o&callback=onMapsApiLoaded');
+    $.getScript('http://maps.google.com/maps/api/js?sensor=false&callback=onMapsApiLoaded');
     alert("mappa letta");
-  },
-  // chiamata quando la posizione è stata letta
-  onMapsApiLoaded: function(){
-    // aggiorna le coordinate
-    alert("Entro nella funzione");
-    var mapOptions = {
-      center: { lat: -34.397, lng: 150.644},
-      zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-    alert("mappa ok");
   }
+}
+
+// chiamata quando la posizione è stata letta
+function onMapsApiLoaded(){
+  // aggiorna le coordinate
+  alert("Entro nella funzione");
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  var mapOptions = {
+    center: myLatlng ,
+    zoom: 8
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  alert("mappa ok");
 }
 
 app.initialize();
