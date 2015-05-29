648910d3d55a249767c3493a8da03cc8b9793441
giotroni <giorgio@troni.it>
2015-05-29 16:45:43 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -10,12 +10,14 @@ var app = {
     document.addEventListener("deviceready", app.onDeviceReady, false);
   },
   onDeviceReady: function(){
-    alert("cerco la posizione");
+    alert("carico la mappa");
+    $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBH7uaEdJNrfDU4RHjgtPg971Fm8pHzZ3o&callback=onMapsApiLoaded');
+    alert("mappa letta");
   },
   // chiamata quando la posizione è stata letta
-  onSuccessGeo: function(position){
+  onMapsApiLoaded: function(){
     // aggiorna le coordinate
-    
+    alert("Entro nella funzione");
     var mapOptions = {
       center: { lat: -34.397, lng: 150.644},
       zoom: 8
@@ -27,4 +29,3 @@ var app = {
 }
 
 app.initialize();
-google.maps.event.addDomListener(window, 'load', onSuccessGeo);
