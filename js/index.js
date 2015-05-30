e51d037219590ee0ebcd59f69ccbbe431e1a82d4
giotroni <giorgio@troni.it>
2015-05-30 08:52:03 +0200

    versione SEMPLICE senza phonegap

--- js/index.js
+++ js/index.js
@@ -1,38 +1,20 @@
-(function (global) {
-    "use strict";
 
-    function onDeviceReady() {
-        document.addEventListener("online", onOnline, false);
-        document.addEventListener("resume", onResume, false);
-        loadMapsApi();
-    }
+var map;
+function onMappaCaricata() {
+  alert("on load");
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  alert("fatto");
+}
+             
+function caricaMappa() {
+  alert("Partito");
+  $("#btnLoad").on("click", onMappaCaricata);    
+}
 
-    function onOnline() {
-        loadMapsApi();
-    }
 
-    function onResume() {
-        loadMapsApi();
-    }
-
-    function loadMapsApi() {
-        if (navigator.connection.type === Connection.NONE || (global.google !== undefined && global.google.maps)) {
-          alert("no connection");
-            return;
-        }
-        alert("connsesso: carico la mappa");
-        //TODO: Add your own Google maps API key to the URL below.
-        $.getScript('https://maps.googleapis.com/maps/api/js?sensor=true&callback=onMapsApiLoaded');
-    }
-
-    global.onMapsApiLoaded = function () {
-        // Maps API loaded and ready to be used.
-        alert("on load");
-        var map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644)
-        });
-    };
-
-    document.addEventListener("deviceready", onDeviceReady, false);
-})(window);
+google.maps.event.addDomListener(window, 'load', caricaMappa);
