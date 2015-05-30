e294b231743b4033ba18dd5b94ed1fcb0872f223
giotroni <giorgio@troni.it>
2015-05-30 13:09:55 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,21 +1,14 @@
 var app = {
 
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
     initialize: function() {
-        app.showAlert("Chiamata al lancio","msg");
+        alert("Chiamata al lancio");
         this.bindEvents();
     },
     bindEvents: function(){
         document.addEventListener("deviceready", app.onDeviceReady, false)
     },
     onDeviceReady: function(){
-        app.showAlert("Chiamata alla fine del caricamento","msg");
+        alert("Chiamata alla fine del caricamento");
     }
 };
 
