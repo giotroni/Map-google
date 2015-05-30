21c7ebbe6e0d106ea40bb4a4ffa0e6f064a4b742
giotroni <giorgio@troni.it>
2015-05-30 15:08:17 +0200

    ok

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var map;
+
 var app = {
 
     initialize: function() {
@@ -8,9 +10,20 @@ var app = {
         document.addEventListener("deviceready", app.onDeviceReady, false)
     },
     onDeviceReady: function(){
-        alert("Chiamata alla fine del caricamento");
+        alert("adesso si carica...");
+        $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBH7uaEdJNrfDU4RHjgtPg971Fm8pHzZ3o&callback=onMapLoaded');
     }
 };
 
+function onMapLoaded(){
+  alert("on load");
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  alert("fatto");
+}
 
 app.initialize();
